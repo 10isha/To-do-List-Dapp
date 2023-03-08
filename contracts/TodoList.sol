30ec1b573416e6d5bd23cba7ca5ec41ca25cabe5
10isha <tanishasahu007@gmail.com>
2023-03-08 14:33:09 +0530

    Adding tasks feature

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,7 +11,11 @@ contract TodoList {
   }
 
   mapping(uint => Task) public tasks;
-
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
 
   constructor() public {
     createTask("List Down your tasks to do");
@@ -20,7 +24,7 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
-    // emit TaskCreated(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
   }
 
 
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -22,5 +22,17 @@ contract('TodoList', (accounts) => {
     
 })
 
+    it('creates tasks',async()=>{
+        const result = await this.todoList.createTask('A new Task')
+        const taskCount = await this.todoList.taskCount()
+        assert.equal(taskCount,2)
+        console.log(result)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(),2)
+        assert.equal(event.content,'A new Task')
+        assert.equal(event.completed,false)
+
+    })
+
 
 })
